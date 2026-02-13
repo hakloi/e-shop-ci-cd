2a4dc16ef31639283e34762229dd264986fab683
Violetta <murasakitta@gmail.com>
2026-02-13 11:18:42 +0300

    Проверка CI

---
+++ test_main.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_health():
+    """Проверка endpoint /health"""
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
+
+def test_products():
+    """Проверка endpoint /products"""
+    response = client.get("/products")
+    
+    products = response.json() 
+    assert response.status_code == 200
+    assert isinstance(products, list)
+    assert len(products) > 0
